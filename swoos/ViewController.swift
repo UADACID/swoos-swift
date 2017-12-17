da1b2f74a228458c87b7e000242415504bad88e0
Pratama <pratamasetya99@gmail.com>
2017-12-17 21:00:38 +0700

    done part 5

--- swoos/ViewController.swift
+++ swoos/ViewController.swift
@@ -14,10 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindToVC1(sender: UIStoryboardSegue) {
+        // this may be blank
     }
 
 
